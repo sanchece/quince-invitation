1b89b970122d536dcb6b64e418525de06df82407
carlos sanchez <sanchece98@gmail.com>
2022-04-15 13:49:13 -0400

    updated countdown

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("June 25, 2022 14:00:00").getTime();
+var countDownDate = new Date("May 28, 2022 14:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
